9f4dc362386463c7dc740b2df3e2c0436a8cb4fe
Samylaneres <samyaneres13@gmail.com>
2024-09-09 09:55:02 -0300

    combustível

---
+++ atividade09.py
@@ -0,0 +1,8 @@
+# Um motorista deseja calcular o consumo médio de combustível do seu carro. Crie um programa que receba a quantidade de quilômetros percorridos e a quantidade de litros de combustível consumidos, e calcule o consumo médio (km por litro).
+Combustível = float(input("Seu gasto de combustível aqui"))
+Litros = float(input("Seus KM percorridos aqui"))
+
+Media = float(Combustível / Litros )
+
+
+print(F"Sua media de consumo é {Media}")
